7805924a70dc72c54533e0ec91c0262c73d4dae2
James Wilson <jw9563@gmail.com>
2023-09-17 15:15:02 +0100

    Player Animations

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7.0f;
+
+
+    private bool isWalking = false;
 
     private void Update()
     {
@@ -32,9 +36,16 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero; 
+
         // rotate to the move direction
         float rotationSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotationSpeed * Time.deltaTime);
 
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
