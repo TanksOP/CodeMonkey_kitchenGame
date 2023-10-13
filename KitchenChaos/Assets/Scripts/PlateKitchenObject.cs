4611eebb62cf634c4923bfa9951347c691e800a8
James Wilson <jw9563@gmail.com>
2023-10-13 17:12:23 +0100

    icons
    added in icons above the plate to show what items are currently on the plate

--- KitchenChaos/Assets/Scripts/PlateKitchenObject.cs
+++ KitchenChaos/Assets/Scripts/PlateKitchenObject.cs
@@ -41,4 +41,10 @@ public class PlateKitchenObject : KitchenObject
         }
         
     }
+
+    public List<KitchenObjectSO> GetKitchenObjectSOList()
+    {
+        return kitchenObjectSOList;
+    }
+        
 }
